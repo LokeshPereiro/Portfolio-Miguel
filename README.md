c96d1cc394f085d3894c840dcd0334668865a409
Lokesh Pereiro <lpdraft001@gmail.com>
2023-04-11 18:37:25 +0200

    vite-tailwindcss config & boilate files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Portfolio-Miguel
-Client frontend portfolio
+
+En este proyecto voy a crear un portfolio ( frontend ) en respuesta a la petición del cliente Miguel Martínez-Falero donde los stack de tecnologías que uso son los siguientes
+
+```
+Maquetación básica: HTML, CSS
+Estilo: Tailwind CSS
+Animaciones e interactividad: Reactjs
+
+```
---
+++ src/components/Nav.jsx
@@ -0,0 +1,46 @@
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex items-center justify-between text-2x1 text-white/50">
+          <Link
+            to="home"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BiHomeAlt />
+          </Link>
+          <Link
+            to="user"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BiUser />
+          </Link>
+          <Link
+            to="dash"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsClipboardData />
+          </Link>
+          <Link
+            to="projects"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsBriefcase />
+          </Link>
+          <Link
+            to="contact"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsChatSquare />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
